a08ab7a73f607affe2530287d6ad02cdefdbc747
Sayed Sofiyan <sofiyan9545@gmail.com>
2024-10-31 13:36:42 +0530

    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
-import Verify from './pages/Verify'
+import Verify from "./pages/Verify"
 
 
 import { ToastContainer, toast } from 'react-toastify';
