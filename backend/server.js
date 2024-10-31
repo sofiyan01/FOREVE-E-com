4a1465205b1bfab7d67edf30407ad476538fb15d
Sayed Sofiyan <sofiyan9545@gmail.com>
2024-10-31 18:46:38 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,55 @@
-import express from "express"
-import cors from "cors"
-import morgan from "morgan"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
-import userRouter from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js";
 import productRouter from "./routes/product.route.js";
-import cartRouter from "./routes/cart.route.js"
-import orderRouter from "./routes/order.route.js"
+import cartRouter from "./routes/cart.route.js";
+import orderRouter from "./routes/order.route.js";
 import connectCloudinary from "./config/cloudinary.js";
+
 dotenv.config();
 
-// app config
-const app=express();
-const port= 4001
+// App config
+const app = express();
+const port = 4001;
+
 connectDB();
 connectCloudinary();
 
 // Middlewares
-app.use(express.json())
-app.use(morgan("common"))
+app.use(express.json());
+app.use(morgan("common"));
 
-app.use(cors()); // allow all origins
+// CORS configuration
+const allowedOrigins = [
+    'https://foreve-e-frontend.vercel.app',
+    'https://foreve-e-admin.vercel.app',
+];
 
+app.use(cors({
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) { // Allow requests with no origin like mobile apps
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // Enable credentials if needed
+}));
 
-// app.use(bodyParser(urlencoded:{true}))
-app.use("/api/user",userRouter)
-app.use("/api/product",productRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
 // API Endpoints
-app.get("/",(req,res)=>{
-    res.json("Working api")
-})
+app.get("/", (req, res) => {
+    res.json("Working api");
+});
 
-
-// listening port
-app.listen(port,()=>{
-        console.log(`server is running on ${port}`)
-})
+// Listening port
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
+});
